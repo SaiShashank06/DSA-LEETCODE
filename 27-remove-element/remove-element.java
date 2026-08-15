b888c0913a949a09f219ed4721bfc44b071e7fe6
PAMULA SAI SHASHANK <shashankpamula2006@gmail.com>
2026-08-16 00:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (62.05%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0,j=nums.length-1;
+        while(i<=j){
+            if(nums[i]==val)
+            {
+               nums[i]=nums[j];
+
+
+                j--;
+            }else {
+                i++;
+            }
+
+        }
+
+           
+     
+           
+       return i; 
+            
+    }
+}
