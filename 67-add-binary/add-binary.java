16b64e0391888f0482844eafe67e0e6df29304d7
PAMULA SAI SHASHANK <shashankpamula2006@gmail.com>
2026-08-16 00:26:18 +0530

    Time: 1 ms (99.94%) | Memory: 43.4 MB (84.60%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder st=new StringBuilder();
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+
+
+
+        while(i>=0 || j>=0 || carry>0){
+            int sum=carry;
+            if(i>=0){
+                sum+=a.charAt(i--)-'0';
+
+            }
+            if(j>=0){
+                sum+=b.charAt(j--)-'0';
+            }
+            st.append(sum%2);
+            carry=sum/2;
+        }
+
+
+        return st.reverse().toString();
+    }
+}
+
+
+/**
+
+Iteration 1
+a[i]=0
+b[j]=1
+
+sum=0+0+1=1
+
+Append
+
+1
+
+carry
+
+0
+st="1"
+ */
